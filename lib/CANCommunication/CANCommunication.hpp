ea05b572514af60fc12431fb054afe885f23189e
Osawa <131154732+oswystk15662@users.noreply.github.com>
2023-09-24 11:36:17 +0900

    v0.95

--- lib/CANCommunication/CANCommunication.hpp
+++ lib/CANCommunication/CANCommunication.hpp
@@ -9,12 +9,16 @@ public:
         //前進がtheta = 0; asin(r)で角度出して、その角度に比例した速度にする。
         void readController(float* jr, float* jtheta, uint8_t* jb);
 
-        void caller(char* msgs);
+        void read_msgs();
     private:
+
+        CAN can;
+
+        char msgs[21];
 
         //最下位バイトが1なら+何周したか、0なら-何周したか、下2byteは2048のうちなんぼか。を４つ分
         char encoder_data[4][4];
 
         //JoyStick_R : 2byte, JoyStick_theta : 2byte, botton : 1byte
-        char msg[5];
+        char ctrler_data[5];
 };
